3dd0a441e00d6b03aa15c089393f071466b9f5a8
lgtromm <102863314+lgtromm@users.noreply.github.com>
2022-05-12 16:36:23 +0900

    Add Day 1 band name generator

---
+++ beginner/day1_band_name_generator.py
@@ -0,0 +1,22 @@
+'''
+Band Name Generator
+Combines the color you're wearing and food you had
+to create band name
+'''
+
+#1. Create a greeting for your program.
+print("Hello! Type your information to create fresh band name!")
+
+#2. Ask the user for the color.
+color = input("What's the color of the bottoms you're wearing right?: \n")
+
+#3. Ask the user for the food.
+food = input("What's the last thing you ate?: \n")
+
+#4. Combine the name of food and color and show them their band name.
+band_name = color +" "+ food
+print(band_name)
+
+#5. Make sure the input cursor shows on a new line
+# added \n on line 11 and 14
+
