bc8c8a042991bf2522a655cb65dffc6f6f54ceb8
lgtromm <102863314+lgtromm@users.noreply.github.com>
2022-05-15 22:05:39 +0900

    day 16

---
+++ intermediate/day16/main.py
@@ -0,0 +1,23 @@
+# import another_module
+# print(another_module.another_variable)
+#
+# # import turtle
+# from turtle import Turtle, Screen
+# # timmy = turtle.Turtle()
+# timmy = Turtle()
+# timmy.shape("turtle")
+# timmy.color("blue")
+# timmy.fd(100)
+#
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
+
+from prettytable import PrettyTable
+table = PrettyTable()
+table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
+table.align = "l"
+
+print(table)
+
